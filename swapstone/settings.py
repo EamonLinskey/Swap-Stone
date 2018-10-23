a521c86eea1068859f7d643429c127ce6b537319
EamonLinskey <Eamon.Linskey@gmail.com>
2018-10-23 11:08:40 -0400

    Added Static Root

--- swapstone/settings.py
+++ swapstone/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefghijklmnopqrstuvwxyz123456789')
 
