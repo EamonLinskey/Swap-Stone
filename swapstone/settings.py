a395aec53f4be831c3e210616f65822b9b05a10d
EamonLinskey <Eamon.Linskey@gmail.com>
2018-10-23 11:51:07 -0400

    updated allowed hosts

--- swapstone/settings.py
+++ swapstone/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefghijklmnopqrstuvwxyz123456789')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [swapstone.herokuapp.com]
+ALLOWED_HOSTS = ['swapstone.herokuapp.com']
 
 
 # Application definition
