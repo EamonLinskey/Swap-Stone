8caf4ea6f044044588a3b826809d9ea048118bfa
EamonLinskey <Eamon.Linskey@gmail.com>
2018-10-25 12:03:34 -0400

    Edited Database config

--- swapstone/settings.py
+++ swapstone/settings.py
@@ -82,8 +82,14 @@ WSGI_APPLICATION = 'swapstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
+DATABASES['default'] =  dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
