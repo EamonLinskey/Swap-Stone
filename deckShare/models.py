f3f84bf20054db8b3a6a2c110a328e0ba2d00c11
EamonLinskey <Eamon.Linskey@gmail.com>
2018-10-25 15:45:53 -0400

    Added players, decks and matches to models

--- deckShare/models.py
+++ deckShare/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Deck(models.Model):
+	name = models.CharField(max_length=50)
+	deckString = models.CharField(max_length=200)
+	Hero = models.CharField(max_length=20)
+
+class Player(models.Model):
+	blizzTag = models.CharField(max_length=100)
+	wishList = models.ManyToManyField(Deck, blank=True)
+	matches = models.ManyToManyField(Match, blank=True)
+
+class Match(models.Model):
+	player1 = models.ForeignKey(Player, on_delete=models.CASCADE)
+	player2 = models.ForeignKey(Player, on_delete=models.CASCADE)
+	deckString1 = models.CharField(max_length=200)
+	deckString2 = models.CharField(max_length=200)
