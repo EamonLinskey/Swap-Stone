20eb7f986e657933a6ef6c57e30b62eb383ba087
EamonLinskey <Eamon.Linskey@gmail.com>
2018-10-25 15:50:18 -0400

    added registered models with admin

--- deckShare/admin.py
+++ deckShare/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Deck, Player, Match
+# Register your models here.
+admin.site.register(Deck)
+admin.site.register(Player)
+admin.site.register(Match)
+
+
 # Register your models here.
